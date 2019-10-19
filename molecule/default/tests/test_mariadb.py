64561e4058c2334ae6501f83a8aa19d334e365fe
Luc Stroobant <luc@warp.be>
2019-10-19 15:08:29 +0200

    various improvements

--- molecule/default/tests/test_mariadb.py
+++ molecule/default/tests/test_mariadb.py
@@ -17,20 +17,8 @@ def test_mariadb_package(host):
 def test_mariadb_server_config(host):
     f = host.file('/etc/mysql/mariadb.conf.d/50-server.cnf')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
-def test_mariadb_logdir(host):
-    d = host.file('/var/log/mysql')
-    assert d.is_directory
-
-
-def test_mariadb_server_innodb_config(host):
     f = host.file('/etc/mysql/conf.d/innodb.cnf')
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
 
 
 def test_mariadb_service(host):
@@ -38,3 +26,12 @@ def test_mariadb_service(host):
     """
     assert host.service('mysql').is_enabled
     assert host.service('mysql').is_running
+
+
+def test_databases(host):
+    """ Test if test databases exist
+    """
+    with host.sudo():
+        dbs = host.run('mysql -u root -e "show databases"')
+        assert 'molecule' in dbs.stdout
+        assert 'molecule2' in dbs.stdout
