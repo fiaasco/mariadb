c2817124ec262675776c40e18bb8400f30ef8865
Luc Stroobant <luc@warp.be>
2020-04-15 21:00:31 +0200

    tests

--- molecule/centos/tests/test_large_prefix.py
+++ molecule/centos/tests/test_large_prefix.py
@@ -2,13 +2,13 @@ testinfra_hosts = ["ansible://mariadb-centos7"]
 
 
 def test_mariadb_el7_config(host):
-    f = host.file('/etc/my.cnf.d/innodb.cnf')
-    assert f.contains('innodb_large_prefix=1')
+    innodb7 = host.file('/etc/my.cnf.d/innodb.cnf')
+    assert not innodb7.contains('innodb_large_prefix=1')
 
 
 testinfra_hosts = ["ansible://mariadb-centos8"]
 
 
 def test_mariadb_el8_config(host):
-    f = host.file('/etc/my.cnf.d/innodb.cnf')
-    assert not f.contains('innodb_large_prefix=1')
+    innodb8 = host.file('/etc/my.cnf.d/innodb.cnf')
+    assert not innodb8.contains('innodb_large_prefix=1')
